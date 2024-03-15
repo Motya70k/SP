397f6573647d5ba53dc0ab8b68c781722865e484
Matvey Shvetsov <matvey.shvetsov@bk.ru>
2024-03-16 00:43:00 +0500

    Complete second practice

---
+++ practice_2/shvetsov_rest_api/dbConnection.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+const db_connection = mysql.createConnection({
+        host: "localhost", // имя хоста
+        user: "root", // имя пользователя
+        database: "pe-11b_shvetsov", // имя базы данных
+        password: "", // пароль от базы данных
+        port: "3307" 
+    })
+    .on("error", (err) => {
+        console.log("Failed to connect to Database - ", err);
+    });
+module.exports = db_connection;
