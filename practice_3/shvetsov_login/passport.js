3e27918a0eac046a7d79dc8e216024c3c9d566bf
Matvey Shvetsov <matvey.shvetsov@bk.ru>
2024-04-16 15:11:12 +0500

    done pr3

--- practice_3/shvetsov_login/passport.js
+++ practice_3/shvetsov_login/passport.js
@@ -10,8 +10,8 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID:"787150806272-5ds4tmacgfaoj4l8fl1fms072tshshc5.apps.googleusercontent.com", // Данные из вашего аккаунта.
-    clientSecret:"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", // Данные из вашего аккаунта.
+    clientID:"", // Данные из вашего аккаунта.
+    clientSecret:"", // Данные из вашего аккаунта.
     callbackURL:"http://localhost:3000/auth/google/callback",
     passReqToCallback:true
     },
@@ -21,8 +21,8 @@ passport.use(new GoogleStrategy({
 ));
 
 passport.use(new GitHubStrategy({
-    clientID: "dc59d957ba23e7a686fb",
-    clientSecret: "3236ce9e4b1342342fe433b442f18ce4bbfdaf32",
+    clientID: "",
+    clientSecret: "",
     callbackURL: "http://localhost:3000/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
